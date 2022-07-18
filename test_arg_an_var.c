bdfa6db6ce260cc4c4eef468595699921c58ad7d
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-07-18 10:37:10 +0200

    Create test_arg_an_var.c

---
+++ test_arg_an_var.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ( int argc, int *args[] ) {
+        int a = 5 ;
+        if ( args[1] )
+                printf("first argument is set\n");
+        if ( a )
+                printf("variable a is set\n");
+}
