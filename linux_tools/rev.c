a818bd1fdc5b0a12df50506eef6724dd7e160471
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-07-27 09:27:38 +0200

    Add files via upload

---
+++ linux_tools/rev.c
@@ -0,0 +1,43 @@
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+
+void do_stdin() ;
+void do_args ( char *args[] ) ;
+
+char line[1024] ;
+
+int main (int count,  char *args[]  ) {
+	do_stdin();
+	do_args ( args ) ;
+}
+
+void do_stdin() {
+	if ( ! isatty( 0 ) ) {
+		while ( fgets( line, 1024, stdin ) ) {
+			for ( int i = (int)strlen(line)-2 ; i >= 0 ; i-- ) {
+				printf( "%c", line[i] );
+			}
+			printf( "%c", line[ (int)strlen(line)-1 ] ) ;
+		}	
+	}	
+}
+
+void do_args ( char *args[] ) {
+	FILE *fp ;
+	int i = 1 ;
+	while ( args[i] ) {
+		fp = fopen( args[i] , "r" );
+		if ( fp ) {
+			while ( fgets( line, 1024, fp ) ) {
+				for ( int i = (int)strlen(line)-2 ; i >= 0 ; i-- ) {
+					printf( "%c", line[i] );
+				}
+				printf( "%c", line[ (int)strlen(line)-1 ] ) ;
+			}
+			fclose( fp );
+		}
+		i++ ;
+	}
+}
+
